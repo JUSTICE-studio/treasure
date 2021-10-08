1a97bc105c4d91771feb3401f345471f18d15a0f
JUSTICE-studio <studiojustice1005@gmail.com>
2021-10-08 21:33:24 +0900

    ang

---
+++ Assets/Scripts/아무의미없는스크.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 아무의미없는스크 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
